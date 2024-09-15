2650977c6e368b2e418f5753a99559edbe8caeb3
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-09-15 14:24:14 +0000

    feat(renderer.c): implement rendering optimization by consolidating   transformation matrices into a single MVP matrix for improved performance   and readability during the rendering process feat(renderer.c): add new output.png file for storing rendered images;   this allows for easy verification of rendering results refactor(renderer.c): simplify data structures by reducing face vertex   indices from MAX_FACE_VERTICES to a fixed array size of 3 for triangles,   enhancing memory efficiency and ensuring only triangle faces are processed

--- renderer.c
+++ renderer.c
@@ -17,39 +17,12 @@
 #define MAX_VERTICES 100000
 #define MAX_TEXCOORDS 100000
 #define MAX_FACES 100000
-#define MAX_FACE_VERTICES 10
 
 // Define vector and matrix structures
-typedef struct {
-    float x, y, z;
-} Vec3f;
-
-typedef struct {
-    float x, y;
-} Vec2f;
-
-typedef struct {
-    float x, y, z, w;
-} Vec4f;
-
-typedef struct {
-    float m[4][4];
-} Mat4f;
-
-// Function prototypes
-Mat4f identMat4f();
-Mat4f multMat4f(Mat4f a, Mat4f b);
-Mat4f translMat4f(float tx, float ty, float tz);
-Mat4f xRotMat4f(float angle);
-Mat4f yRotMat4f(float angle);
-Mat4f zRotMat4f(float angle);
-Mat4f scaleMat4f(float sx, float sy, float sz);
-Mat4f modelMat4f(float tx, float ty, float tz, float rx, float ry, float rz, float sx, float sy, float sz);
-Mat4f create_projection_matrix(float fov, float aspect_ratio, float near, float far);
-Mat4f create_view_matrix(Vec3f eye, Vec3f center, Vec3f up);
-void parse_obj_file(const char *file_path);
-float edge_function(Vec2f v0, Vec2f v1, Vec2f p);
-void render_triangles(int width, int height, Mat4f model_matrix, Mat4f view_matrix, Mat4f projection_matrix);
+typedef struct { float x, y, z; } Vec3f;
+typedef struct { float x, y; } Vec2f;
+typedef struct { float x, y, z, w; } Vec4f; // Added Vec4f definition
+typedef struct { float m[4][4]; } Mat4f;
 
 // Global variables for vertices, texture coordinates, faces, and textures
 Vec3f vertices[MAX_VERTICES];
@@ -59,9 +32,8 @@ Vec2f texture_coords[MAX_TEXCOORDS];
 int num_texture_coords = 0;
 
 typedef struct {
-    int vertex_indices[MAX_FACE_VERTICES];
-    int texture_indices[MAX_FACE_VERTICES];
-    int num_vertices;
+    int vertex_indices[3];
+    int texture_indices[3];
 } Face;
 
 Face faces[MAX_FACES];
@@ -75,31 +47,28 @@ float *depth_buffer;
 
 // Vector and matrix utility functions
 Vec3f vec3f_sub(Vec3f a, Vec3f b) {
-    Vec3f result = {a.x - b.x, a.y - b.y, a.z - b.z};
-    return result;
+    return (Vec3f){ a.x - b.x, a.y - b.y, a.z - b.z };
 }
 
 Vec3f vec3f_normalize(Vec3f v) {
-    float length = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-    Vec3f result = {v.x / length, v.y / length, v.z / length};
-    return result;
+    float length = sqrtf(v.x*v.x + v.y*v.y + v.z*v.z);
+    return (Vec3f){ v.x / length, v.y / length, v.z / length };
 }
 
 Vec3f vec3f_cross(Vec3f a, Vec3f b) {
-    Vec3f result = {
-        a.y * b.z - a.z * b.y,
-        a.z * b.x - a.x * b.z,
-        a.x * b.y - a.y * b.x
+    return (Vec3f){
+        a.y*b.z - a.z*b.y,
+        a.z*b.x - a.x*b.z,
+        a.x*b.y - a.y*b.x
     };
-    return result;
 }
 
 float vec3f_dot(Vec3f a, Vec3f b) {
-    return a.x * b.x + a.y * b.y + a.z * b.z;
+    return a.x*b.x + a.y*b.y + a.z*b.z;
 }
 
 Vec4f mat4f_mul_vec4f(Mat4f mat, Vec4f vec) {
-    Vec4f result;
+    Vec4f result = {0};
     result.x = mat.m[0][0]*vec.x + mat.m[0][1]*vec.y + mat.m[0][2]*vec.z + mat.m[0][3]*vec.w;
     result.y = mat.m[1][0]*vec.x + mat.m[1][1]*vec.y + mat.m[1][2]*vec.z + mat.m[1][3]*vec.w;
     result.z = mat.m[2][0]*vec.x + mat.m[2][1]*vec.y + mat.m[2][2]*vec.z + mat.m[2][3]*vec.w;
@@ -110,91 +79,67 @@ Vec4f mat4f_mul_vec4f(Mat4f mat, Vec4f vec) {
 // Matrix transformation functions
 Mat4f identMat4f() {
     Mat4f mat = {0};
-    mat.m[0][0] = 1.0f;
-    mat.m[1][1] = 1.0f;
-    mat.m[2][2] = 1.0f;
-    mat.m[3][3] = 1.0f;
+    mat.m[0][0] = mat.m[1][1] = mat.m[2][2] = mat.m[3][3] = 1.0f;
     return mat;
 }
 
 Mat4f multMat4f(Mat4f a, Mat4f b) {
     Mat4f result = {0};
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-            result.m[i][j] = 0;
-            for(int k = 0; k < 4; k++) {
+    for (int i = 0; i < 4; ++i)
+        for (int j = 0; j < 4; ++j)
+            for (int k = 0; k < 4; ++k)
                 result.m[i][j] += a.m[i][k]*b.m[k][j];
-            }
-        }
-    }
     return result;
 }
 
 Mat4f translMat4f(float tx, float ty, float tz) {
     Mat4f mat = identMat4f();
-    mat.m[0][3] = tx;
-    mat.m[1][3] = ty;
-    mat.m[2][3] = tz;
+    mat.m[0][3] = tx; mat.m[1][3] = ty; mat.m[2][3] = tz;
     return mat;
 }
 
 Mat4f xRotMat4f(float angle) {
-    float c = cosf(angle);
-    float s = sinf(angle);
+    float c = cosf(angle), s = sinf(angle);
     Mat4f mat = identMat4f();
-    mat.m[1][1] = c;
-    mat.m[1][2] = -s;
-    mat.m[2][1] = s;
-    mat.m[2][2] = c;
+    mat.m[1][1] = c; mat.m[1][2] = -s;
+    mat.m[2][1] = s; mat.m[2][2] = c;
     return mat;
 }
 
 Mat4f yRotMat4f(float angle) {
-    float c = cosf(angle);
-    float s = sinf(angle);
+    float c = cosf(angle), s = sinf(angle);
     Mat4f mat = identMat4f();
-    mat.m[0][0] = c;
-    mat.m[0][2] = s;
-    mat.m[2][0] = -s;
-    mat.m[2][2] = c;
+    mat.m[0][0] = c; mat.m[0][2] = s;
+    mat.m[2][0] = -s; mat.m[2][2] = c;
     return mat;
 }
 
 Mat4f zRotMat4f(float angle) {
-    float c = cosf(angle);
-    float s = sinf(angle);
+    float c = cosf(angle), s = sinf(angle);
     Mat4f mat = identMat4f();
-    mat.m[0][0] = c;
-    mat.m[0][1] = -s;
-    mat.m[1][0] = s;
-    mat.m[1][1] = c;
+    mat.m[0][0] = c; mat.m[0][1] = -s;
+    mat.m[1][0] = s; mat.m[1][1] = c;
     return mat;
 }
 
 Mat4f scaleMat4f(float sx, float sy, float sz) {
     Mat4f mat = identMat4f();
-    mat.m[0][0] = sx;
-    mat.m[1][1] = sy;
-    mat.m[2][2] = sz;
+    mat.m[0][0] = sx; mat.m[1][1] = sy; mat.m[2][2] = sz;
     return mat;
 }
 
 Mat4f modelMat4f(float tx, float ty, float tz, float rx, float ry, float rz, float sx, float sy, float sz) {
-    Mat4f modelmatrix = identMat4f();
-    modelmatrix = multMat4f(translMat4f(tx, ty, tz), modelmatrix);
+    Mat4f translation = translMat4f(tx, ty, tz);
     Mat4f rotation = multMat4f(multMat4f(xRotMat4f(rx), yRotMat4f(ry)), zRotMat4f(rz));
-    modelmatrix = multMat4f(rotation, modelmatrix);
-    modelmatrix = multMat4f(scaleMat4f(sx, sy, sz), modelmatrix);
-    return modelmatrix;
+    Mat4f scale = scaleMat4f(sx, sy, sz);
+    return multMat4f(translation, multMat4f(rotation, scale));
 }
 
 Mat4f create_projection_matrix(float fov, float aspect_ratio, float near, float far) {
     float f = 1.0f / tanf(fov / 2.0f);
     Mat4f mat = {0};
-    mat.m[0][0] = f / aspect_ratio;
-    mat.m[1][1] = f;
-    mat.m[2][2] = (far + near) / (near - far);
-    mat.m[2][3] = (2 * far * near) / (near - far);
+    mat.m[0][0] = f / aspect_ratio; mat.m[1][1] = f;
+    mat.m[2][2] = (far + near) / (near - far); mat.m[2][3] = (2 * far * near) / (near - far);
     mat.m[3][2] = -1.0f;
     return mat;
 }
@@ -203,77 +148,44 @@ Mat4f create_view_matrix(Vec3f eye, Vec3f center, Vec3f up) {
     Vec3f f = vec3f_normalize(vec3f_sub(center, eye));
     Vec3f s = vec3f_normalize(vec3f_cross(f, up));
     Vec3f u = vec3f_cross(s, f);
-
     Mat4f mat = identMat4f();
 
-    mat.m[0][0] = s.x;
-    mat.m[0][1] = s.y;
-    mat.m[0][2] = s.z;
-    mat.m[0][3] = -vec3f_dot(s, eye);
-
-    mat.m[1][0] = u.x;
-    mat.m[1][1] = u.y;
-    mat.m[1][2] = u.z;
-    mat.m[1][3] = -vec3f_dot(u, eye);
-
-    mat.m[2][0] = -f.x;
-    mat.m[2][1] = -f.y;
-    mat.m[2][2] = -f.z;
-    mat.m[2][3] = vec3f_dot(f, eye);
-
-    mat.m[3][0] = 0.0f;
-    mat.m[3][1] = 0.0f;
-    mat.m[3][2] = 0.0f;
-    mat.m[3][3] = 1.0f;
-
+    mat.m[0][0] = s.x; mat.m[0][1] = s.y; mat.m[0][2] = s.z; mat.m[0][3] = -vec3f_dot(s, eye);
+    mat.m[1][0] = u.x; mat.m[1][1] = u.y; mat.m[1][2] = u.z; mat.m[1][3] = -vec3f_dot(u, eye);
+    mat.m[2][0] = -f.x; mat.m[2][1] = -f.y; mat.m[2][2] = -f.z; mat.m[2][3] = vec3f_dot(f, eye);
     return mat;
 }
 
 // OBJ file parsing function
 void parse_obj_file(const char *file_path) {
     FILE *file = fopen(file_path, "r");
-    if (!file) {
-        printf("Failed to open OBJ file %s\n", file_path);
-        exit(1);
-    }
+    if (!file) { printf("Failed to open OBJ file %s\n", file_path); exit(1); }
 
     char line[256];
     while (fgets(line, sizeof(line), file)) {
         if (strncmp(line, "v ", 2) == 0) {
-            // Vertex position
             Vec3f vertex;
             sscanf(line + 2, "%f %f %f", &vertex.x, &vertex.y, &vertex.z);
             vertices[num_vertices++] = vertex;
         } else if (strncmp(line, "vt ", 3) == 0) {
-            // Texture coordinate
             Vec2f texcoord;
             sscanf(line + 3, "%f %f", &texcoord.x, &texcoord.y);
             texture_coords[num_texture_coords++] = texcoord;
         } else if (strncmp(line, "f ", 2) == 0) {
-            // Face
             Face face;
-            face.num_vertices = 0;
             char *token = strtok(line + 2, " \n");
-            while (token) {
+            int idx = 0;
+            while (token && idx < 3) {
                 int vi = 0, ti = 0;
-                if (strstr(token, "/")) {
-                    // Format: vertex_index/texture_index
-                    sscanf(token, "%d/%d", &vi, &ti);
-                } else {
-                    // Only vertex indices
-                    sscanf(token, "%d", &vi);
-                }
-                if (vi < 0) vi += num_vertices + 1;
-                if (ti < 0) ti += num_texture_coords + 1;
-                face.vertex_indices[face.num_vertices] = vi - 1;
-                face.texture_indices[face.num_vertices] = ti - 1;
-                face.num_vertices++;
+                sscanf(token, "%d/%d", &vi, &ti);
+                face.vertex_indices[idx] = vi - 1;
+                face.texture_indices[idx] = ti - 1;
+                idx++;
                 token = strtok(NULL, " \n");
             }
             faces[num_faces++] = face;
         }
     }
-
     fclose(file);
 }
 
@@ -283,131 +195,80 @@ float edge_function(Vec2f v0, Vec2f v1, Vec2f p) {
 }
 
 // Triangle rendering function
-void render_triangles(int width, int height, Mat4f model_matrix, Mat4f view_matrix, Mat4f projection_matrix) {
-    image_data = (unsigned char *)malloc(width * height * 3);
-    memset(image_data, 0, width * height * 3);
+void render_triangles(int width, int height, Mat4f mvp_matrix) {
+    image_data = (unsigned char *)calloc(width * height * 3, sizeof(unsigned char));
     depth_buffer = (float *)malloc(width * height * sizeof(float));
-    for (int i = 0; i < width * height; i++) {
-        depth_buffer[i] = INFINITY;
-    }
-
-    Mat4f view_model_matrix = multMat4f(view_matrix, model_matrix);
+    for (int i = 0; i < width * height; i++) depth_buffer[i] = INFINITY;
 
     for (int face_idx = 0; face_idx < num_faces; face_idx++) {
         Face face = faces[face_idx];
 
-        // Only process triangles
-        if (face.num_vertices != 3) {
-            continue;
+        Vec4f v_homogeneous[3];
+        Vec2f vt[3];
+
+        for (int i = 0; i < 3; i++) {
+            Vec3f v = vertices[face.vertex_indices[i]];
+            vt[i] = texture_coords[face.texture_indices[i]];
+            v_homogeneous[i] = (Vec4f){ v.x, v.y, v.z, 1.0f };
+            v_homogeneous[i] = mat4f_mul_vec4f(mvp_matrix, v_homogeneous[i]);
+            v_homogeneous[i].x /= v_homogeneous[i].w;
+            v_homogeneous[i].y /= v_homogeneous[i].w;
+            v_homogeneous[i].z /= v_homogeneous[i].w;
+            v_homogeneous[i].x = (v_homogeneous[i].x + 1.0f) * 0.5f * width;
+            v_homogeneous[i].y = (1.0f - v_homogeneous[i].y) * 0.5f * height;
         }
 
-        // Get vertices and texture coordinates
-        Vec3f v0 = vertices[face.vertex_indices[0]];
-        Vec3f v1 = vertices[face.vertex_indices[1]];
-        Vec3f v2 = vertices[face.vertex_indices[2]];
+        // Bounding box
+        float min_x = fminf(fminf(v_homogeneous[0].x, v_homogeneous[1].x), v_homogeneous[2].x);
+        float max_x = fmaxf(fmaxf(v_homogeneous[0].x, v_homogeneous[1].x), v_homogeneous[2].x);
+        float min_y = fminf(fminf(v_homogeneous[0].y, v_homogeneous[1].y), v_homogeneous[2].y);
+        float max_y = fmaxf(fmaxf(v_homogeneous[0].y, v_homogeneous[1].y), v_homogeneous[2].y);
 
-        Vec2f vt0 = texture_coords[face.texture_indices[0]];
-        Vec2f vt1 = texture_coords[face.texture_indices[1]];
-        Vec2f vt2 = texture_coords[face.texture_indices[2]];
+        int x0 = fmaxf(0, floorf(min_x));
+        int x1 = fminf(width - 1, ceilf(max_x));
+        int y0 = fmaxf(0, floorf(min_y));
+        int y1 = fminf(height - 1, ceilf(max_y));
 
-        // Apply view-model matrix
-        Vec4f v0_h = {v0.x, v0.y, v0.z, 1.0f};
-        Vec4f v1_h = {v1.x, v1.y, v1.z, 1.0f};
-        Vec4f v2_h = {v2.x, v2.y, v2.z, 1.0f};
+        Vec2f v_screen[3] = {
+            { v_homogeneous[0].x, v_homogeneous[0].y },
+            { v_homogeneous[1].x, v_homogeneous[1].y },
+            { v_homogeneous[2].x, v_homogeneous[2].y }
+        };
 
-        v0_h = mat4f_mul_vec4f(view_model_matrix, v0_h);
-        v1_h = mat4f_mul_vec4f(view_model_matrix, v1_h);
-        v2_h = mat4f_mul_vec4f(view_model_matrix, v2_h);
+        float area = edge_function(v_screen[0], v_screen[1], v_screen[2]);
 
-        // Apply projection matrix
-        v0_h = mat4f_mul_vec4f(projection_matrix, v0_h);
-        v1_h = mat4f_mul_vec4f(projection_matrix, v1_h);
-        v2_h = mat4f_mul_vec4f(projection_matrix, v2_h);
-
-        // Perspective division
-        v0_h.x /= v0_h.w;
-        v0_h.y /= v0_h.w;
-        v0_h.z /= v0_h.w;
-
-        v1_h.x /= v1_h.w;
-        v1_h.y /= v1_h.w;
-        v1_h.z /= v1_h.w;
-
-        v2_h.x /= v2_h.w;
-        v2_h.y /= v2_h.w;
-        v2_h.z /= v2_h.w;
-
-        // Convert to screen space
-        v0_h.x = (v0_h.x + 1.0f) * width / 2.0f;
-        v0_h.y = (1.0f - v0_h.y) * height / 2.0f;
-
-        v1_h.x = (v1_h.x + 1.0f) * width / 2.0f;
-        v1_h.y = (1.0f - v1_h.y) * height / 2.0f;
-
-        v2_h.x = (v2_h.x + 1.0f) * width / 2.0f;
-        v2_h.y = (1.0f - v2_h.y) * height / 2.0f;
-
-        // Prepare for rasterization
-        Vec2f v0_screen = {v0_h.x, v0_h.y};
-        Vec2f v1_screen = {v1_h.x, v1_h.y};
-        Vec2f v2_screen = {v2_h.x, v2_h.y};
-
-        // Calculate bounding box
-        int min_x = fmaxf(0, floorf(fminf(fminf(v0_screen.x, v1_screen.x), v2_screen.x)));
-        int max_x = fminf(width - 1, ceilf(fmaxf(fmaxf(v0_screen.x, v1_screen.x), v2_screen.x)));
-        int min_y = fmaxf(0, floorf(fminf(fminf(v0_screen.y, v1_screen.y), v2_screen.y)));
-        int max_y = fminf(height - 1, ceilf(fmaxf(fmaxf(v0_screen.y, v1_screen.y), v2_screen.y)));
-
-        // Rasterize the triangle
-        for (int y = min_y; y <= max_y; y++) {
-            for (int x = min_x; x <= max_x; x++) {
-                Vec2f p = {(float)x + 0.5f, (float)y + 0.5f};
-
-                float w0 = edge_function(v1_screen, v2_screen, p);
-                float w1 = edge_function(v2_screen, v0_screen, p);
-                float w2 = edge_function(v0_screen, v1_screen, p);
+        for (int y = y0; y <= y1; y++) {
+            for (int x = x0; x <= x1; x++) {
+                Vec2f p = { x + 0.5f, y + 0.5f };
+                float w0 = edge_function(v_screen[1], v_screen[2], p);
+                float w1 = edge_function(v_screen[2], v_screen[0], p);
+                float w2 = edge_function(v_screen[0], v_screen[1], p);
 
                 if (w0 >= 0 && w1 >= 0 && w2 >= 0) {
-                    // Calculate area
-                    float area = edge_function(v0_screen, v1_screen, v2_screen);
-                    // Barycentric coordinates
-                    w0 /= area;
-                    w1 /= area;
-                    w2 /= area;
+                    w0 /= area; w1 /= area; w2 /= area;
 
-                    // Interpolate depth
-                    float depth = w0 * v0_h.z + w1 * v1_h.z + w2 * v2_h.z;
+                    float depth = w0*v_homogeneous[0].z + w1*v_homogeneous[1].z + w2*v_homogeneous[2].z;
 
-                    // Depth test
-                    int index = y * width + x;
-                    if (depth < depth_buffer[index]) {
-                        depth_buffer[index] = depth;
+                    int idx = y * width + x;
+                    if (depth < depth_buffer[idx]) {
+                        depth_buffer[idx] = depth;
 
-                        // Interpolate texture coordinates
-                        float tx = w0 * vt0.x + w1 * vt1.x + w2 * vt2.x;
-                        float ty = 1.0f - (w0 * vt0.y + w1 * vt1.y + w2 * vt2.y); // Invert v coordinate
+                        // Texture coordinates interpolation
+                        float u = w0*vt[0].x + w1*vt[1].x + w2*vt[2].x;
+                        float v = 1.0f - (w0*vt[0].y + w1*vt[1].y + w2*vt[2].y);
 
-                        // Sample texture color
-                        int tex_x = (int)(tx * texture_width) % texture_width;
-                        int tex_y = (int)(ty * texture_height) % texture_height;
-                        int tex_index = (tex_y * texture_width + tex_x) * texture_channels;
+                        int tex_x = (int)(u * texture_width) % texture_width;
+                        int tex_y = (int)(v * texture_height) % texture_height;
+                        int tex_idx = (tex_y * texture_width + tex_x) * texture_channels;
 
-                        unsigned char r = texture_data[tex_index];
-                        unsigned char g = texture_data[tex_index + 1];
-                        unsigned char b = texture_data[tex_index + 2];
-
-                        // Set pixel color
-                        int img_index = (y * width + x) * 3;
-                        image_data[img_index] = r;
-                        image_data[img_index + 1] = g;
-                        image_data[img_index + 2] = b;
+                        image_data[idx * 3 + 0] = texture_data[tex_idx];
+                        image_data[idx * 3 + 1] = texture_data[tex_idx + 1];
+                        image_data[idx * 3 + 2] = texture_data[tex_idx + 2];
                     }
                 }
             }
         }
     }
-
-    // Free depth buffer
     free(depth_buffer);
 }
 
@@ -435,14 +296,16 @@ int main() {
         (Vec3f){0, 1, 0}
     );
     Mat4f projection_matrix = create_projection_matrix(
-        (float)(M_PI / 4.0f), // 45 degrees in radians
+        M_PI / 4.0f, // 45 degrees in radians
         (float)width / (float)height,
         0.1f,
         100.0f
     );
 
+    Mat4f mvp_matrix = multMat4f(projection_matrix, multMat4f(view_matrix, model_matrix));
+
     // Render triangles
-    render_triangles(width, height, model_matrix, view_matrix, projection_matrix);
+    render_triangles(width, height, mvp_matrix);
 
     // Save image
     if (!stbi_write_png(output_file, width, height, 3, image_data, width * 3)) {
