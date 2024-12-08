6f8c12dd2dbe6681c7c643767d62733f77b4c819
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-12-08 16:51:49 +0000

    refactor(rasterizer): update parse_obj_file and render_frame to accept dynamic arrays and texture parameters

--- rasterizer.c
+++ rasterizer.c
@@ -24,20 +24,16 @@
 #define VEC_SCALE(v,s,r) { (r)[0]=(v)[0]*(s); (r)[1]=(v)[1]*(s); (r)[2]=(v)[2]*(s); }
 #define VEC_ADD(a,b,r) { (r)[0]=(a)[0]+(b)[0]; (r)[1]=(a)[1]+(b)[1]; (r)[2]=(a)[2]+(b)[2]; }
 
-// Global variables
-double vertices[100000][3];
-double initial_vertices[100000][3];
-double texcoords[100000][2];
-int triangles[200000][3];
-int texcoord_indices[200000][3];
-int num_vertices = 0, num_texcoords = 0, num_triangles = 0;
-
-// Texture data
-unsigned char *texture_data = NULL;
-int texture_width, texture_height, texture_channels;
-
 // Function to parse OBJ file
-void parse_obj_file(const char *filename) {
+void parse_obj_file(const char *filename, 
+                   double (*vertices)[3], 
+                   double (*initial_vertices)[3],
+                   double (*texcoords)[2],
+                   int (*triangles)[3],
+                   int (*texcoord_indices)[3],
+                   int *num_vertices,
+                   int *num_texcoords,
+                   int *num_triangles) {
     FILE *file = fopen(filename, "r");
     if (!file) { fprintf(stderr, "Failed to open OBJ file %s\n", filename); exit(1); }
 
@@ -46,14 +42,14 @@ void parse_obj_file(const char *filename) {
         if (strncmp(line, "v ", 2) == 0) {
             double x, y, z;
             sscanf(line + 2, "%lf %lf %lf", &x, &y, &z);
-            vertices[num_vertices][0] = x; vertices[num_vertices][1] = y; vertices[num_vertices][2] = z;
-            initial_vertices[num_vertices][0] = x; initial_vertices[num_vertices][1] = y; initial_vertices[num_vertices][2] = z;
-            num_vertices++;
+            vertices[*num_vertices][0] = x; vertices[*num_vertices][1] = y; vertices[*num_vertices][2] = z;
+            initial_vertices[*num_vertices][0] = x; initial_vertices[*num_vertices][1] = y; initial_vertices[*num_vertices][2] = z;
+            (*num_vertices)++;
         } else if (strncmp(line, "vt ", 3) == 0) {
             double u, v;
             sscanf(line + 3, "%lf %lf", &u, &v);
-            texcoords[num_texcoords][0] = u; texcoords[num_texcoords][1] = v;
-            num_texcoords++;
+            texcoords[*num_texcoords][0] = u; texcoords[*num_texcoords][1] = v;
+            (*num_texcoords)++;
         } else if (strncmp(line, "f ", 2) == 0) {
             int vi[3], ti[3];
             int matches = sscanf(line + 2, "%d/%d %d/%d %d/%d", &vi[0], &ti[0], &vi[1], &ti[1], &vi[2], &ti[2]);
@@ -65,11 +61,13 @@ void parse_obj_file(const char *filename) {
                     else { ti[0] = ti[1] = ti[2] = -1; }
                 }
             }
-            triangles[num_triangles][0] = vi[0] - 1; triangles[num_triangles][1] = vi[1] - 1; triangles[num_triangles][2] = vi[2] - 1;
-            texcoord_indices[num_triangles][0] = (ti[0] != -1) ? ti[0] - 1 : -1;
-            texcoord_indices[num_triangles][1] = (ti[1] != -1) ? ti[1] - 1 : -1;
-            texcoord_indices[num_triangles][2] = (ti[2] != -1) ? ti[2] - 1 : -1;
-            num_triangles++;
+            triangles[*num_triangles][0] = vi[0] - 1;
+            triangles[*num_triangles][1] = vi[1] - 1;
+            triangles[*num_triangles][2] = vi[2] - 1;
+            texcoord_indices[*num_triangles][0] = (ti[0] != -1) ? ti[0] - 1 : -1;
+            texcoord_indices[*num_triangles][1] = (ti[1] != -1) ? ti[1] - 1 : -1;
+            texcoord_indices[*num_triangles][2] = (ti[2] != -1) ? ti[2] - 1 : -1;
+            (*num_triangles)++;
         }
     }
     fclose(file);
@@ -82,7 +80,10 @@ void rotate_y(double angle, double point[3]) {
 }
 
 // Sample texture
-void sample_texture(double u, double v, double color[3]) {
+void sample_texture(double u, double v, double color[3],
+                   unsigned char *texture_data,
+                   int texture_width,
+                   int texture_height) {
     // Clamp and wrap UV coordinates
     u = fmod(u, 1.0); if (u < 0) u += 1.0;
     v = fmod(v, 1.0); if (v < 0) v += 1.0;
@@ -105,8 +106,10 @@ void sample_texture(double u, double v, double color[3]) {
     color[2] = texture_data[idx + 2] / 255.0;
 }
 
-// Function to draw a filled triangle using the barycentric coordinate method
-void draw_triangle(double *image, const double pts[3][4], const double uv[3][2]) {
+void draw_triangle(double *image, const double pts[3][4], const double uv[3][2],
+                  unsigned char *texture_data,
+                  int texture_width,
+                  int texture_height) {
     double bbox_min_x = fmin(fmin(pts[0][0], pts[1][0]), pts[2][0]);
     double bbox_min_y = fmin(fmin(pts[0][1], pts[1][1]), pts[2][1]);
     double bbox_max_x = fmax(fmax(pts[0][0], pts[1][0]), pts[2][0]);
@@ -115,20 +118,23 @@ void draw_triangle(double *image, const double pts[3][4], const double uv[3][2])
     for (int x = (int)fmax(bbox_min_x, 0); x <= (int)fmin(bbox_max_x, WIDTH - 1); x++) {
         for (int y = (int)fmax(bbox_min_y, 0); y <= (int)fmin(bbox_max_y, HEIGHT - 1); y++) {
             double lambda[3];
-            double denominator = ((pts[1][1] - pts[2][1]) * (pts[0][0] - pts[2][0]) + (pts[2][0] - pts[1][0]) * (pts[0][1] - pts[2][1]));
-            lambda[0] = ((pts[1][1] - pts[2][1]) * (x - pts[2][0]) + (pts[2][0] - pts[1][0]) * (y - pts[2][1])) / denominator;
-            lambda[1] = ((pts[2][1] - pts[0][1]) * (x - pts[2][0]) + (pts[0][0] - pts[2][0]) * (y - pts[2][1])) / denominator;
+            double denominator = ((pts[1][1] - pts[2][1]) * (pts[0][0] - pts[2][0]) + 
+                                (pts[2][0] - pts[1][0]) * (pts[0][1] - pts[2][1]));
+            lambda[0] = ((pts[1][1] - pts[2][1]) * (x - pts[2][0]) + 
+                        (pts[2][0] - pts[1][0]) * (y - pts[2][1])) / denominator;
+            lambda[1] = ((pts[2][1] - pts[0][1]) * (x - pts[2][0]) + 
+                        (pts[0][0] - pts[2][0]) * (y - pts[2][1])) / denominator;
             lambda[2] = 1.0 - lambda[0] - lambda[1];
 
             if (lambda[0] >= 0 && lambda[0] <= 1 && lambda[1] >= 0 && lambda[1] <= 1 && lambda[2] >= 0 && lambda[2] <= 1) {
                 double z = lambda[0] * pts[0][2] + lambda[1] * pts[1][2] + lambda[2] * pts[2][2];
                 int idx = y * WIDTH + x;
                 if (z < image[idx * 4 + 3]) {
-                    image[idx * 4 + 3] = z; // update depth
+                    image[idx * 4 + 3] = z;
                     double u = lambda[0] * uv[0][0] + lambda[1] * uv[1][0] + lambda[2] * uv[2][0];
                     double v = lambda[0] * uv[0][1] + lambda[1] * uv[1][1] + lambda[2] * uv[2][1];
                     double color[3];
-                    sample_texture(u, v, color);
+                    sample_texture(u, v, color, texture_data, texture_width, texture_height);
                     image[idx * 4] = color[0];
                     image[idx * 4 + 1] = color[1];
                     image[idx * 4 + 2] = color[2];
@@ -138,8 +144,19 @@ void draw_triangle(double *image, const double pts[3][4], const double uv[3][2])
     }
 }
 
-void render_frame(uint8_t *image, int frame_num, 
-                 double scale_factor, double translation[3], double angle_per_frame) {
+void render_frame(uint8_t *image, int frame_num,
+                 double scale_factor, double translation[3],
+                 double angle_per_frame,
+                 double (*vertices)[3],
+                 double (*initial_vertices)[3],
+                 double (*texcoords)[2],
+                 int (*triangles)[3],
+                 int (*texcoord_indices)[3],
+                 int num_vertices,
+                 int num_triangles,
+                 unsigned char *texture_data,
+                 int texture_width,
+                 int texture_height) {
     // Allocate depth buffer
     double *depth_buffer = malloc(WIDTH * HEIGHT * sizeof(double));
     for (int i = 0; i < WIDTH * HEIGHT; i++) {
@@ -203,7 +220,7 @@ void render_frame(uint8_t *image, int frame_num,
                         double u = lambda[0] * uv_coords[0][0] + lambda[1] * uv_coords[1][0] + lambda[2] * uv_coords[2][0];
                         double v = lambda[0] * uv_coords[0][1] + lambda[1] * uv_coords[1][1] + lambda[2] * uv_coords[2][1];
                         double color[3];
-                        sample_texture(u, v, color);
+                        sample_texture(u, v, color, texture_data, texture_width, texture_height);
                         image[idx * 3] = (uint8_t)(color[0] * 255.0);
                         image[idx * 3 + 1] = (uint8_t)(color[1] * 255.0);
                         image[idx * 3 + 2] = (uint8_t)(color[2] * 255.0);
@@ -217,9 +234,46 @@ void render_frame(uint8_t *image, int frame_num,
 }
 
 int main() {
-    parse_obj_file("drone.obj");
+    // Declare arrays with fixed sizes
+    double (*vertices)[3] = malloc(100000 * sizeof(*vertices));
+    double (*initial_vertices)[3] = malloc(100000 * sizeof(*initial_vertices));
+    double (*texcoords)[2] = malloc(100000 * sizeof(*texcoords));
+    int (*triangles)[3] = malloc(200000 * sizeof(*triangles));
+    int (*texcoord_indices)[3] = malloc(200000 * sizeof(*texcoord_indices));
+    
+    if (!vertices || !initial_vertices || !texcoords || !triangles || !texcoord_indices) {
+        fprintf(stderr, "Failed to allocate memory for mesh data\n");
+        exit(1);
+    }
+
+    int num_vertices = 0, num_texcoords = 0, num_triangles = 0;
+    unsigned char *texture_data = NULL;
+    int texture_width = 0, texture_height = 0, texture_channels = 0;
+
+    // Parse OBJ file
+    parse_obj_file("drone.obj", 
+                   vertices, 
+                   initial_vertices,
+                   texcoords,
+                   triangles,
+                   texcoord_indices,
+                   &num_vertices,
+                   &num_texcoords,
+                   &num_triangles);
+
+    // Load texture
     texture_data = load_bmp("drone.bmp", &texture_width, &texture_height, &texture_channels);
+    if (!texture_data) {
+        fprintf(stderr, "Failed to load texture\n");
+        exit(1);
+    }
+
+    // Allocate image buffer
     uint8_t *image = malloc(WIDTH * HEIGHT * 3);
+    if (!image) {
+        fprintf(stderr, "Failed to allocate image buffer\n");
+        exit(1);
+    }
 
     uint8_t palette[8 * 3] = {
         0x00, 0x00, 0x00, // Black
@@ -233,19 +287,43 @@ int main() {
     };
 
     ge_GIF *gif = ge_new_gif("output_rasterizer.gif", WIDTH, HEIGHT, palette, 3, -1, 0);
+    if (!gif) {
+        fprintf(stderr, "Failed to create GIF\n");
+        exit(1);
+    }
 
     double scale_factor = 1.0;
     double translation[3] = {0, 1, 3};
     double angle_per_frame = (2.0 * M_PI) / FRAMES;
 
+    // Render frames
     for (int frame_num = 0; frame_num < FRAMES; frame_num++) {
         printf("Rendering frame %d/%d\n", frame_num + 1, FRAMES);
-        render_frame(image, frame_num, scale_factor, translation, angle_per_frame);
+        render_frame(image, frame_num, 
+                    scale_factor, translation, 
+                    angle_per_frame,
+                    vertices,
+                    initial_vertices,
+                    texcoords,
+                    triangles,
+                    texcoord_indices,
+                    num_vertices,
+                    num_triangles,
+                    texture_data,
+                    texture_width,
+                    texture_height);
         ge_add_frame(gif, image, 6);
     }
 
+    // Cleanup
     ge_close_gif(gif);
     free(image);
     free(texture_data);
+    free(vertices);
+    free(initial_vertices);
+    free(texcoords);
+    free(triangles);
+    free(texcoord_indices);
+
     return 0;
 }
